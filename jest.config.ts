e8d43fb361ff979d2c4748fd2411ae52b6a8bc50
Natã <natan.danilo@gmail.com>
2023-01-28 17:45:20 -0300

    chore: add new config jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,19 @@
  * https://jestjs.io/docs/configuration
  */
 
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
 export default {
   clearMocks: true,
   collectCoverage: true,
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
-  coverageProvider: "v8"
+  // coverageProvider: "v8"
+
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // the following line is needed in order to grab modules from the
+  // src folder without the need to write them relatively
+  moduleDirectories: ["node_modules", "src"]
 }
---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignupController } from './signup'
+
+describe('Signup Controller', () => {
+  test('Should return 400 if name is provided', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        // name: 'any_name',
+        email: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
