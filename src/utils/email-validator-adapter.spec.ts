8f47c0e87f33a4abd75ad1b04945d210173938b0
Natã <natan.danilo@gmail.com>
2023-01-30 20:22:09 -0300

    test: ensure EmailValidator return true if validator return true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { EmailValidatorAdapter } from "./email-validator-adapter"
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+    isEmail(): boolean {
+        return true;
+    }
+}))
 
 describe('EmailValidator Adapter',()=> {
     test('should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter()
-        const emailInvalid = sut.isValid('invalid_email@gmail.com')
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+        const emailInvalid = sut.isValid('emailInvalid@gmail')
         expect(emailInvalid).toBe(false)
+    })
+
+    test('should return false if validator returns true', () => {
+        const sut = new EmailValidatorAdapter()
+        const emailInvalid = sut.isValid('emailValid@gmail.com')
+        expect(emailInvalid).toBe(true)
     })
 })
