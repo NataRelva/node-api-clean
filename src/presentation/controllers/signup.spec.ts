82ca4063faca7a1bf2eca8d7683b40a4524bbb5d
Natã <natan.danilo@gmail.com>
2023-01-29 21:53:48 -0300

    test: ensure SignUpController calls EmailValidator with corrent email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -119,4 +119,21 @@ describe('Signup Controller', () => {
     expect(httpResponse?.statusCode).toBe(400)
     expect(httpResponse?.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct email', ()=> {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: { 
+        name: 'any_name',
+        email: 'valid_email@email.com',
+        password: 'any_password',
+        phone: 'any_phone',
+        cpfCnpj: 'any_cpfCnpj'
+      }
+    }
+
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
+  })
 })
