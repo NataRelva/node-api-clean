52cba7f8158d061016c723eef9a230561691bc43
Natã <natan.danilo@gmail.com>
2023-01-30 21:22:24 -0300

    feat: ensure implement CpfCnpjValidation in controller

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -2,17 +2,22 @@ import { Controller, HttpRequest, HttpResponse, EmailValidator, AddAccount } fro
 import { MissingParamError, InvalidParamError } from '../../errors/index'
 import { badRequest, serverError } from '../../helpers/http.helper'
 import { ok } from '../../helpers/http.helper'
+import { CpfCnpjValidator } from '../../protocols/cpf-cnpj-validator'
 
 export class SignupController implements Controller {
 
   private readonly emailValidator: EmailValidator
   private readonly addAccountStub: AddAccount
+  private readonly cpfCnpjValidator: CpfCnpjValidator
+
   constructor(
     emailValidator: EmailValidator,
-    addAccount: AddAccount
+    addAccount: AddAccount,
+    cpfCnpjValidator: CpfCnpjValidator
   ) {
     this.emailValidator = emailValidator
     this.addAccountStub = addAccount
+    this.cpfCnpjValidator = cpfCnpjValidator
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -21,12 +26,14 @@ export class SignupController implements Controller {
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
       }
+
       const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
-      
       if (!emailIsValid) return badRequest(new InvalidParamError('email'))
+
+      const cpfOrCpnjIsValid = this.cpfCnpjValidator.isValid(httpRequest.body.cpfCnpj)
+      if (!cpfOrCpnjIsValid) return badRequest(new InvalidParamError('cpjCpnj'))
       
       const account = await this.addAccountStub.add(httpRequest.body)
-
       return ok(account)
 
     } catch (error) {
---
+++ src/presentation/protocols/cpf-cnpj-validator.ts
@@ -0,0 +1,3 @@
+export interface CpfCnpjValidator {
+    isValid(cpfCnpj: string): boolean
+}
