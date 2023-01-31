83e69a511f7058502a6cf2970018a4eb5b56561f
Natã <natan.danilo@gmail.com>
2023-01-31 01:04:41 -0300

    test: enseure BcryptAdapter return a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,11 +1,22 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+    async hash(): Promise<string> {
+        return new Promise(res => res('anythingHash'))
+    }
+}))
+
 describe('Bcrypt Adapter', () => {
     test('Should call bcrypt with correct value', async () => {
         const sut = new BcryptAdapter(12)
         const hashSpy = jest.spyOn(bcrypt, 'hash')
         await sut.encrypt('anything')
         expect(hashSpy).toHaveBeenCalledWith('anything', 12)
+    })
+    test('Should return a hash on success', async () => {
+        const sut = new BcryptAdapter(12)
+        const hash = await sut.encrypt('anything')
+        expect(hash).toBe('anythingHash')
     })
 })
