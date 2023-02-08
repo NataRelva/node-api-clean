9f70c37d93023853413b5774cf775b44c74efffa
Natã <natan.danilo@gmail.com>
2023-02-07 22:59:10 -0300

    feat: ensure add Authentication useCase

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -3,13 +3,18 @@ import { Controller, HttpRequest, HttpResponse } from "../signup/signup-protocol
 import { EmailValidator } from "../signup/signup-protocols";
 import { badRequest, serverError } from "../../helpers/http.helper";
 import { InvalidParamError } from "../../errors/invalid-param.error";
+import { Authentication } from "../../../domain/useCases/authentication";
 
 export class LoginController implements Controller {
     private readonly emailValidator: EmailValidator;
+    private readonly authentication: Authentication;
+
     constructor(
         emailValidator: EmailValidator,
+        authentication: Authentication
     ) {
         this.emailValidator = emailValidator;
+        this.authentication = authentication;
     }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const requiredFields = ['email', 'password'];
@@ -20,6 +25,8 @@ export class LoginController implements Controller {
         try {
             const isValidEmail = this.emailValidator.isValid(email);
             if (!isValidEmail) return badRequest(new InvalidParamError('email'));
+            const isPasswordValid = await this.authentication.auth(email, password);
+            if (!isPasswordValid) return badRequest(new InvalidParamError('password'));
             return await new Promise(resolve => resolve({ statusCode: 200, body: {} }));
         } catch (error) {
             return serverError();
