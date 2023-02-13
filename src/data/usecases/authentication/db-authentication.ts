33bd621caf0c9918d22006033f87b4d1798523c7
Natã <natan.danilo@gmail.com>
2023-02-12 23:45:56 -0300

    feat: ensure DbAuthentication return a token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,21 +1,31 @@
 import { Authentication, AuthenticationModel } from "../../../domain/useCases/authentication";
 import { HashComparer } from "../../protocols/criptography/hash-comparer";
+import { TokenGeneration } from "../../protocols/criptography/token-generation";
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email.repository";
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
     private readonly hashComparer: HashComparer
+    private readonly tokenGeneration: TokenGeneration
     constructor(
         loadAccountByEmail: LoadAccountByEmailRepository,
-        hashComparer: HashComparer) {
+        hashComparer: HashComparer,
+        tokenGeneration: TokenGeneration
+    ) {
         this.loadAccountByEmailRepository = loadAccountByEmail
         this.hashComparer = hashComparer
+        this.tokenGeneration = tokenGeneration
     }
 
     async auth(authentication: AuthenticationModel): Promise<string | null> {
         const account = await this.loadAccountByEmailRepository.load(authentication.email)
-        if (!account) return null
-        await this.hashComparer.compare(authentication.password, account.password)
+        if (account) {
+            const isValid = await this.hashComparer.compare(authentication.password, account.password)
+            if (isValid) {
+                const accesToken = await this.tokenGeneration.generate(account.id)
+                return accesToken
+            }
+        }
         return null
     }
 }
