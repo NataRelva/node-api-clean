9100448408a2fd9a26ccc4cbe364634bea655ca9
Natã <natan.danilo@gmail.com>
2023-02-13 00:01:36 -0300

    refactor: move DbAuthenticatioj protocol to a single file

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,9 +1,5 @@
-import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email.repository"
-import { AccountModel } from "../add-account/db-add-account.protocols"
+import { LoadAccountByEmailRepository, HashComparer, TokenGeneration, UpdateAccessTokenRepository, AccountModel } from "./db-authentication.protocols"
 import { DbAuthentication } from "./db-authentication"
-import { HashComparer } from "../../protocols/criptography/hash-comparer"
-import { TokenGeneration } from "../../protocols/criptography/token-generation"
-import { UpdateAccessTokenRepository } from "../../protocols/db/update-access-token.repository"
 
 interface TypeSut {
     sut: DbAuthentication,
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,8 +1,4 @@
-import { Authentication, AuthenticationModel } from "../../../domain/useCases/authentication";
-import { HashComparer } from "../../protocols/criptography/hash-comparer";
-import { TokenGeneration } from "../../protocols/criptography/token-generation";
-import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email.repository";
-import { UpdateAccessTokenRepository } from "../../protocols/db/update-access-token.repository";
+import { LoadAccountByEmailRepository, HashComparer, TokenGeneration, UpdateAccessTokenRepository, AuthenticationModel, Authentication } from "./db-authentication.protocols"
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
