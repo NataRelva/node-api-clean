44e44313af8736d248cf805ff0ce32dffde6a977
Natã <natan.danilo@gmail.com>
2023-02-14 15:35:54 -0300

    bug: fix implementation error response

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
+import { makeLoginController } from './../factories/login/login-factorie';
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adpter";
 import { makeSignupController } from "../factories/singup/singup"
 
 export default function (router: Router): void {
     router.post("/singup", adaptRoute(makeSignupController()));
+    router.post("/login", adaptRoute(makeLoginController()));
 }
--- src/presentation/errors/unauthorized-error.ts
+++ src/presentation/errors/unauthorized-error.ts
@@ -1,6 +1,6 @@
 export class UnauthorizedError extends Error {
-    constructor(msg?: string) {
-        if (msg) super(msg)
-        else super('Internal server error')
+    constructor() {
+        super('Unauthorized')
+        this.name = 'UnauthorizedError'
     }
 } 
