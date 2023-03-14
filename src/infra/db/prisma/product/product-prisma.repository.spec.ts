111f8bdcab2fb705057906046436a44f500f6d39
Natã <natan.danilo@gmail.com>
2023-03-14 20:41:16 -0300

    test: ensure full cycle produts rmoura

--- src/infra/db/prisma/product/product-prisma.repository.spec.ts
+++ src/infra/db/prisma/product/product-prisma.repository.spec.ts
@@ -1,8 +1,9 @@
+import { RmouraProduct } from './../../../../domain/useCases/register-rmoura-product';
 import { RmouraProductModel } from './../../../../domain/models/rmoura-product';
 import { PrismaClient } from '@prisma/client';
 import { ProductPrismaRepository } from './product-prisma.repository';
 
-const makeFakeRmouraProducts = (): any => { 
+const makeFakeRmouraProducts = (): any => {
   return [{
     name: 'any_name',
     weight: 1,
@@ -14,135 +15,57 @@ const makeFakeRmouraProducts = (): any => {
   }]
 }
 
-jest.mock('@prisma/client', () => { 
-  return {
-    PrismaClient: jest.fn().mockImplementation(() => { 
-      return {
-        rMouraProduct: {
-          create: jest.fn().mockImplementation(() => { 
-            return {
-              id: 'any_id',
-              name: 'any_name',
-              weight: 1,
-              unit: 'any_unit',
-              obs: 'any_obs',
-              package: 'any_package',
-              price: 1,
-              categories: 'any_categories',
-            }
-          }),
-          findUnique: jest.fn().mockImplementation(() => {
-            return {
-              id: 'any_id',
-              name: 'any_name',
-              weight: 1,
-              unit: 'any_unit',
-              obs: 'any_obs',
-              package: 'any_package',
-              price: 1,
-              categories: 'any_categories',
-            }
-          }),
-          deleteMany: jest.fn().mockImplementation(() => { 
-            return {
-              count: 1
-            }
-          })
-        },
-        rMouraCategory: { 
-          findUnique: jest.fn().mockImplementation(() => { 
-            return {
-              id: 'any_id',
-              name: 'any_name',
-            }
-          }),
-        },
-        rMouraUnit: { 
-          findUnique: jest.fn().mockImplementation(() => { 
-            return {
-              id: 'any_id',
-              name: 'any_name',
-            }
-          }),
-        },
-        rMouraPackage: { 
-          findUnique: jest.fn().mockImplementation(() => {
-            return {
-              id: 'any_id',
-              name: 'any_name',
-            }
-          })
-        },
-        rMouraCategoryProduct: {
-          create: jest.fn().mockImplementation(() => { 
-            return {
-              id: 'any_id',
-              rMouraProductId: 'any_product_id',
-              rMouraCategoryId: 'any_category_id',
-            }
-          })
-        },
-        rMouraUnitProduct: { 
-          create: jest.fn().mockImplementation(() => { 
-            return {
-              id: 'any_id',
-              rMouraProductId: 'any_product_id',
-              rMouraUnitId: 'any_unit_id',
-            }
-          })
-        },
-        rMouraPackageProduct: { 
-          create: jest.fn().mockImplementation(() => {
-            return {
-              id: 'any_id',
-              rMouraProductId: 'any_product_id',
-              rMouraPackageId: 'any_package_id',
-            }
-          })
-        }
-      }
-    })
-  }
-})
 
-const makePrismaStub = (): PrismaClient => { 
-  return new PrismaClient();
+
+const makePrismaStub = (): PrismaClient => {
+  class PrismaClientStub extends PrismaClient {
+    constructor() {
+      super();
+    }
+  }
+  return new PrismaClientStub();
 }
+
 
 interface SutTypes {
   sut: ProductPrismaRepository;
   prismaStub: PrismaClient;
 }
-const makeSut = (): SutTypes => { 
+const makeSut = (): SutTypes => {
   const prismaStub = makePrismaStub();
   const sut = new ProductPrismaRepository(prismaStub);
   return { sut, prismaStub };
 }
 
-describe('ProductPrismaRepository', () => { 
-  test('Should call PrismaClient with correct values', async () => { 
+describe('ProductPrismaRepository', () => {
+  test('Should call PrismaClient with correct values', async () => {
     const { sut, prismaStub } = makeSut();
-    const addSpy = jest.spyOn(prismaStub.rMouraProduct, 'create');
+    const createSpy = jest.spyOn(prismaStub.rmouraProduct, 'create');
     await sut.addRmoura(makeFakeRmouraProducts());
-    expect(addSpy).toHaveBeenCalledWith({
-      data: { 
-        name: 'any_name',
-        weight: 1,
-        obs: 'any_obs',
-        price: 1,
-      }
+    expect(createSpy).toHaveBeenCalledWith({ 
+      "data": { 
+        "category": { "connectOrCreate": { "create": { "name": "any_package" }, "where": { "name": "any_package" } } }, 
+        "package": { "connectOrCreate": { "create": { "name": "any_package" }, "where": { "name": "any_package" } } }, 
+        "unit": { "connectOrCreate": { "create": { "name": "any_unit" }, "where": { "name": "any_unit" } } }, 
+        "name": "any_name",
+        "obs": "any_obs", 
+        "price": 1, 
+        "weight": 1 
+      } 
     })
+
+
   })
 
-  test('Should return an RmouraProductModel on success', async () => { 
+  test('Should return an RmouraProductModel on success', async () => {
     const { sut } = makeSut();
     const response = await sut.addRmoura(makeFakeRmouraProducts());
     expect(response).toEqual(undefined)
   })
 
-  test('Should throw if PrismaClient throws', async () => { 
+  test('Should throw if PrismaClient throws', async () => {
     const { sut, prismaStub } = makeSut();
-    jest.spyOn(prismaStub.rMouraProduct, 'create').mockImplementationOnce(() => { 
+    jest.spyOn(prismaStub.rmouraProduct, 'create').mockImplementationOnce(() => {
       throw new Error();
     })
     const promise = sut.addRmoura(makeFakeRmouraProducts());
--- src/presentation/controllers/pull-products-rmoura/pull-products-rmoura-controller.spec.ts
+++ src/presentation/controllers/pull-products-rmoura/pull-products-rmoura-controller.spec.ts
@@ -1,4 +1,4 @@
-import { RmouraProduct } from './../../../domain/useCases/register-rmoura-product';
+import { RmouraProductModel } from './../../../domain/models/rmoura-product';
 import { FilterRequest } from './../../../domain/models/product-configuration';
 import { PullProductsRmouraController } from './pull-products-rmoura-controller';
 import { HttpRequest } from './../../protocols/http';
@@ -7,21 +7,44 @@ import { PullProductsRmoura } from './../../../domain/useCases/pull-products-rmo
 import { ErrorHandlerAdapter } from './../../../utils/error-handler-adapter';
 import { ok, badRequest } from './../../helpers/http/http.helper';
 
-const makeFakeProducts = (): RmouraProduct[] => { 
+const makeFakeProducts = (): RmouraProductModel[] => {
   return [{
-    code: 100,
+    id: 'any_id',
     name: 'any_name',
-    weight: 100,
-    obs: 'any_obs',
+    weight: 0,
     unit: 'any_unit',
-    price: 100,
+    obs: 'any_obs',
     package: 'any_package',
+    price: 100,
+    categories: []
   }]
+}
+
+const makeRequest = (): HttpRequest => { 
+  const httpRequest: HttpRequest = {
+    body: {
+      "filter": {
+        "unitId": "b4cde26a-46bd-4804-bfa5-e711993e233d",
+        "categoryId": "",
+        "packageId": "",
+        "price": {
+          "min": 0,
+          "max": 2000
+        }
+      },
+      "paginator": {
+        "page": 1,
+        "limit": 10
+      },
+      "text": "a"
+    }
+  }
+  return httpRequest
 }
 
 const makeSut = () => {
   class PullProductsRmouraStub implements PullProductsRmoura {
-    async pull(filter: FilterRequest): Promise<RmouraProduct[]> {
+    async pull(filter: FilterRequest): Promise<RmouraProductModel[]> {
       return new Promise((resolve) => resolve(makeFakeProducts()))
     }
   }
@@ -34,54 +57,44 @@ const makeSut = () => {
   return { sut, pullProductsRmouraStub, handleErrorStub }
 }
 
-describe('PullProductsRmouraController', () => { 
-  test('Should call PullProductsRmoura with correct values', async () => { 
+describe('PullProductsRmouraController', () => {
+  test('Should call PullProductsRmoura with correct values', async () => {
     const { sut, pullProductsRmouraStub } = makeSut()
     const pullSpy = jest.spyOn(pullProductsRmouraStub, 'pull')
     const httpRequest: HttpRequest = {
       body: {
-        filter: {
-          code: 100,
-          name: 'any_name',
-          weight: 100,
-          obs: 'any_obs',
-          unit: 'any_unit',
-          price: 100,
-          package: 'any_package',
-        }
+        "filter": {
+          "unitId": "b4cde26a-46bd-4804-bfa5-e711993e233d",
+          "categoryId": "",
+          "packageId": "",
+          "price": {
+            "min": 0,
+            "max": 2000
+          }
+        },
+        "paginator": {
+          "page": 1,
+          "limit": 10
+        },
+        "text": "a"
       }
     }
     await sut.handle(httpRequest)
-    expect(pullSpy).toHaveBeenCalledWith(httpRequest.body.filter)
+    expect(pullSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
-  test('Should return 400 if no filter is provided', async () => { 
+  test('Should return 400 if no filter is provided', async () => {
     const { sut } = makeSut()
     const httpRequest: HttpRequest = {
-      body: {
-        filter: undefined
-      }
+      body: undefined
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('filter')))
   })
 
-  test('Should return 200 if valid data is provided', async () => { 
+  test('Should return 200 if valid data is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest: HttpRequest = {
-      body: {
-        filter: {
-          code: 100,
-          name: 'any_name',
-          weight: 100,
-          obs: 'any_obs',
-          unit: 'any_unit',
-          price: 100,
-          package: 'any_package',
-        }
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(makeRequest())
     expect(httpResponse).toEqual(ok(makeFakeProducts()))
   })
 })
