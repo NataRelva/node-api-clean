ff2dafd25e72290b4dc4d0749fd5f180d51dca0c
Natã <natan.danilo@gmail.com>
2023-03-14 20:41:59 -0300

    chore: change model prisma

--- prisma/migrations/20230313170521_change_name_unic/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name]` on the table `RMouraCategory` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "RMouraCategory_name_key" ON "RMouraCategory"("name");
