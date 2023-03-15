e75a2482e1e4058e684df4605dd5b63dda87dbaa
Natã <natan.danilo@gmail.com>
2023-03-15 12:29:19 -0300

    test: ensure add celmar products repository

---
+++ src/data/usecases/register-celmar-products/db-add-celmar-products-repository.spec.ts
@@ -0,0 +1,43 @@
+import { RequestCelmarProduct } from './../../../domain/useCases/register-products-celmar';
+import { AddCelmarProductsRepository } from './../../protocols/db/product/add-celmar-products.repository';
+import { DBAddCelmarProductsRepository } from './db-add-celmar-products-repository';
+
+interface SutTypes {
+  sut: DBAddCelmarProductsRepository;
+  addCelmarProductsRepositoryStub: AddCelmarProductsRepository;
+}
+
+const makeFakeRequest = (): RequestCelmarProduct[] => { 
+  return [{
+    code: 'any_code',
+    name: 'any_name',
+    price: 10,
+    package: 'any_package',
+    category_main: 'any_category_main',
+    category_sub:  'any_category_sub',
+  }]
+}
+
+const makeAddCelmarProductsRepository = (): AddCelmarProductsRepository => { 
+  class AddCelmarProductsRepositoryStub implements AddCelmarProductsRepository {
+    async addCelmar(data: RequestCelmarProduct[]): Promise<void> {
+      return new Promise(resolve => resolve());
+    }
+  }
+  return new AddCelmarProductsRepositoryStub();
+}
+
+const makeSut = (): SutTypes => { 
+  const addCelmarProductsRepositoryStub = makeAddCelmarProductsRepository();
+  const sut = new DBAddCelmarProductsRepository(addCelmarProductsRepositoryStub);
+  return { sut, addCelmarProductsRepositoryStub };
+}
+
+describe('DbAddCelmarProductsRepository', () => { 
+  it('should call AddCelmarProductsRepository with correct values', async () => { 
+    const { sut, addCelmarProductsRepositoryStub } = makeSut();
+    const addCelmarSpy = jest.spyOn(addCelmarProductsRepositoryStub, 'addCelmar');
+    await sut.register(makeFakeRequest());
+    expect(addCelmarSpy).toHaveBeenCalledWith(makeFakeRequest());
+  })
+})
