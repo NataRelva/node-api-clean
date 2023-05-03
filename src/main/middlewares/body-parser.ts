b71f99957b6e2266ed437a40c66595c981d63c1d
Natã <natan.danilo@gmail.com>
2023-05-03 12:43:25 -0300

    fix: change configs

--- src/main/middlewares/body-parser.ts
+++ src/main/middlewares/body-parser.ts
@@ -1,4 +1,4 @@
 import { json, urlencoded } from 'body-parser';
 
-export const bodyParser = json({limit: '1000mb'})
-export const urlEncoded = urlencoded({limit: '1000mb', extended: true})
+export const bodyParser = json({limit: '10000mb'})
+export const urlEncoded = urlencoded({limit: '10000mb', extended: true})
